4d58baf42fb2ca2c039f5c2cf73ccd753d6e483a
ankitha0911 <161472042+ankitha0911@users.noreply.github.com>
2024-06-27 15:49:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+var express=require("express")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/Database')
+var db=mongoose.connection
+db.on('error',()=> console.log("Error in Connecting to Database"))
+db.once('open',()=> console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res) => {
+    var Username= req.body.Usename
+    var Age=req.body.Age
+    var Email=req.body.Email
+    var Password=req.body.Password
+    var Styling=req.body.Styling
+    var Responsive Design=req.body.Responsive Design
+
+    var data={
+        "Username":Username,
+        "Age":Age,
+        "Email":Email,
+        "Password":Password,
+        "Styling":Styling,
+        "Responsive Design":Responsive Design
+    }
+    db.collection('users').insertOne(data,(err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Succesfully")
+    })
+    return res.redirect('signup_successful.html')
+})
+
+app.get("/",(req,res) => {
+    res.set({
+        "Allow-acces-Allow-Origin":'*'
+    })
+    return res.redirect('index.html')
+}).listen(3000);
+
+console.log("Listening on port 3000")
